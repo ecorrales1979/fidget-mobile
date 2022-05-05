350959ee3c1f60548422fed7010ac767785a964f
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-05-05 19:51:26 -0300

    feat: remove unused text and style status bar

--- App.tsx
+++ App.tsx
@@ -8,10 +8,11 @@ export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: theme.colors.background }}>
       <Widget />
-      <Text style={{ color: theme.colors.text_primary}}>
-        Open up App.tsx to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
+      <StatusBar
+        style="light"
+        backgroundColor='transparent'
+        translucent
+      />
     </View>
   );
 }
