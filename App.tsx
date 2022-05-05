231537d0736eaeaac219b2526f066d6c529f826f
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-05-05 20:25:25 -0300

    feat: load Inter font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
+import AppLoading from 'expo-app-loading';
 import { View } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter';
 
 import { Widget } from './src/components/Widget';
 import { theme } from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={{ flex: 1, backgroundColor: theme.colors.background }}>
       <Widget />
