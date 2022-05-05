b0e69d1ca6eda1e03aacef4d720ed9b1e2173e83
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-05-05 19:50:50 -0300

    feat: add basic widget structure

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
+import { Widget } from './src/components/Widget';
 import { theme } from './src/theme';
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: theme.colors.background }}>
+      <Widget />
       <Text style={{ color: theme.colors.text_primary}}>
         Open up App.tsx to start working on your app!
       </Text>
