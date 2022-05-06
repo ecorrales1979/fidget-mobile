2d22af3d3263c0b9da4863ae3fa9e89571ed374c
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-05-05 22:31:43 -0300

    feat: add feedback options buttons

--- src/components/Option/styles.ts
+++ src/components/Option/styles.ts
@@ -4,18 +4,23 @@ import { theme } from "../../theme";
 
 export const styles = StyleSheet.create({
   container: {
+    width: 104,
+    height: 112,
+    justifyContent: "center",
     alignItems: "center",
+    padding: 8,
+    borderRadius: 8,
+    marginHorizontal: 8,
+    backgroundColor: theme.colors.surface_secondary,
+  },
+  image: {
+    width: 40,
+    height: 40,
   },
   title: {
-    fontSize: 20,
+    fontSize: 14,
+    marginTop: 8,
     fontFamily: theme.fonts.medium,
     color: theme.colors.text_primary,
-    marginBottom: 32,
-  },
-  options: {
-    flexDirection: "row",
-    width: "100%",
-    marginBottom: 48,
-    justifyContent: "center",
   },
 });
--- src/components/Options/index.tsx
+++ src/components/Options/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Copyright } from '../Copyright';
+import { Text, View } from 'react-native';
 
 import { styles } from './styles';
+import { Copyright } from '../Copyright';
+import { Option } from '../Option';
+import { feedbackTypes } from '../../utils/feedbackTypes';
 
 export function Options() {
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Give your feedback</Text>
+
+      <View style={styles.options}>
+        {Object.entries(feedbackTypes).map(([key, value]) => (
+          <Option key={key} image={value.image} title={value.title} />
+        ))}
+      </View>
+
       <Copyright />
     </View>
   );
