cdc3901378ed8527b3332c581afc1a3e80a8daf6
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-05-05 22:33:54 -0300

    fix: translate feedback types to English

--- src/utils/feedbackTypes.ts
+++ src/utils/feedbackTypes.ts
@@ -1,15 +1,14 @@
 export const feedbackTypes = {
-  'BUG': {
-    title: 'Problema',
-    image: require('../assets/bug.png')
+  bug: {
+    title: "Problem",
+    image: require("../assets/bug.png"),
   },
-  'IDEA': {
-    title: 'Ideia',
-    image: require('../assets/idea.png'),
+  idea: {
+    title: "Idea",
+    image: require("../assets/idea.png"),
   },
-  'OTHER': {
-    title: 'Outro',
-    image: require('../assets/thought.png'),
+  other: {
+    title: "Other",
+    image: require("../assets/thought.png"),
   },
 };
-
